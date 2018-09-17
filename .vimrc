345dd06dc8828a801c74c5c61792e921af0c453a
sgz <219253+sangonz@users.noreply.github.com>
2018-09-17 13:16:54 +0200

    Add vim

---
+++ .vimrc
@@ -0,0 +1,4 @@
+syn on
+set t_Co=256
+colorscheme gruvbox
+
--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@
 #
 #
 # Guide to manage dotfiles: https://medium.com/@webprolific/getting-started-with-dotfiles-43c3602fd789
+set -e
 
+P_CHK="\e[32m✓\e[0m"
+P_ERR="\e[31m✗\e[0m"
 
 export DOTFILES_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -11,17 +14,25 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Get the directory with a tilde instead of full path
 dotfile_tilde=$(cd "$DOTFILES_DIR" && dirs +0)
 
+# Install .bash_profile
 if [[ -r ~/.bash_profile && `grep "$dotfile_tilde/.bash_profile" ~/.bash_profile` ]]; then
-    echo ".bash_profile already installed"
+    printf "$P_ERR .bash_profile\n"
 else
-    echo "Installing in current .bash_profile"
     echo ". $dotfile_tilde/.bash_profile" >> ~/.bash_profile
+    printf "$P_CHK .bash_profile\n"
 fi
 
+# Install .gitconfig
 if [[ -r ~/.gitconfig && `grep "path = $dotfile_tilde/gitalias.txt" ~/.gitconfig` ]]; then
-    echo "~/.gitconfig already contains the alias"
+    printf "$P_ERR .gitconfig\n"
 else
-    echo "Installing .gitconfig alias"
     echo "[include]" >> ~/.gitconfig
     echo "    path = $dotfile_tilde/gitalias.txt" >> ~/.gitconfig
+    printf "$P_CHK .gitconfig\n"
 fi
+
+# Install vim
+ln -s $DOTFILES_DIR/.vim ~/.vim 
+printf "$P_CHK .vim/\n"
+ln -s $DOTFILES_DIR/.vimrc ~/.vimrc
+printf "$P_CHK .vimrc\n"
