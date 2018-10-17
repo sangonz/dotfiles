c8f41a00be06b8546f62da01d4c29006eab8aed6
sgz <219253+sangonz@users.noreply.github.com>
2018-10-17 12:17:53 +0200

    Fix install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@
 # Guide to manage dotfiles: https://medium.com/@webprolific/getting-started-with-dotfiles-43c3602fd789
 set -e
 
-P_CHK="\e[32m✓\e[0m"
-P_ERR="\e[31m✗\e[0m"
+P_CHK="\e[32m[ok]\e[0m"
+P_ERR="\e[31m[no]\e[0m"
 
 export DOTFILES_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -14,25 +14,43 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Get the directory with a tilde instead of full path
 dotfile_tilde=$(cd "$DOTFILES_DIR" && dirs +0)
 
-# Install .bash_profile
-if [[ -r ~/.bash_profile && `grep "$dotfile_tilde/.bash_profile" ~/.bash_profile` ]]; then
-    printf "$P_ERR .bash_profile\n"
+function install {
+    file="$1"
+    search="$2"
+    toadd="$3"
+    [[ -z $toadd ]] && toadd="$search"
+    
+    # Caution: ~ doesn't expand within quotes
+    if [[ -r ~/$file && `grep "$search" ~/$file` ]]; then
+        printf "$P_ERR $file\n"
+    else
+        printf "$toadd" >> ~/$file
+        printf "$P_CHK $file\n"
+    fi
+}
+
+
+# Install .bashrc or .bash_profile alternatively
+if [ -r ~/.bashrc ]; then
+    install .bashrc ". $dotfile_tilde/.bash_profile"
 else
-    echo ". $dotfile_tilde/.bash_profile" >> ~/.bash_profile
-    printf "$P_CHK .bash_profile\n"
+    install .bash_profile ". $dotfile_tilde/.bash_profile"
 fi
+
 
 # Install .gitconfig
-if [[ -r ~/.gitconfig && `grep "path = $dotfile_tilde/gitalias.txt" ~/.gitconfig` ]]; then
-    printf "$P_ERR .gitconfig\n"
-else
-    echo "[include]" >> ~/.gitconfig
-    echo "    path = $dotfile_tilde/gitalias.txt" >> ~/.gitconfig
-    printf "$P_CHK .gitconfig\n"
-fi
+install .gitconfig "path = $dotfile_tilde/gitalias.txt" "[include]\n    path = $dotfile_tilde/gitalias.txt"
 
 # Install vim
-ln -s $DOTFILES_DIR/.vim ~/.vim 
-printf "$P_CHK .vim/\n"
-ln -s $DOTFILES_DIR/.vimrc ~/.vimrc
-printf "$P_CHK .vimrc\n"
+function install_ln {
+    file="$1"
+    if [[ -r ~/$file ]]; then
+        printf "$P_ERR $file\n"
+    else
+        ln -s "$DOTFILES_DIR/$file" ~/$file
+        printf "$P_CHK $file\n"
+    fi
+}
+
+install_ln .vim
+install_ln .vimrc
