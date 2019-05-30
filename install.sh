5cc83e102ff15800b99f6e4269408340bef61a4b
sgz <219253+sangonz@users.noreply.github.com>
2019-05-30 12:29:54 +0200

    Mod install.sh

--- install.sh
+++ install.sh
@@ -1,35 +1,32 @@
 #!/usr/bin/env bash
 #
-#
 # Guide to manage dotfiles: https://medium.com/@webprolific/getting-started-with-dotfiles-43c3602fd789
+#
 set -e
 
-P_CHK="\e[32m[ok]\e[0m"
-P_ERR="\e[31m[no]\e[0m"
+P_CHK="\e[32m  ✔ \e[0m"
+P_NEW="\e[32m★ ✔ \e[0m"
+P_ERR="\e[31m  ✘ \e[0m"
 
-export DOTFILES_DIR
-DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Get the directory with a tilde instead of full path
-dotfile_tilde=$(cd "$DOTFILES_DIR" && dirs +0)
-
-
-# Install .gitconfig
-# install .gitconfig "path = $dotfile_tilde/gitalias.txt" "[include]\n    path = $dotfile_tilde/gitalias.txt"
-
-# Install vim
 function install_ln {
     file="$1"
-    dotfile=".$file"
-    if [[ -e ~/$dotfile ]]; then
-        printf "$P_ERR $file\n"
+    target=".dotfiles/$file"
+    dest="$HOME/.$file"
+    if [[ ! -e $dest ]]; then
+        ln -s $target $dest
+        printf "$P_NEW $dest\n"
     else
-        ln -s "$DOTFILES_DIR/$file" ~/$dotfile
-        printf "$P_CHK $dotfile\n"
+        if [[ $(readlink -e $dest) == $(readlink -e $file) ]]; then
+            printf "$P_CHK $dest\n"
+        else
+            printf "$P_ERR $dest\n"
+        fi
     fi
 }
 
 install_ln bashrc
+install_ln bash_profile
 install_ln vim
 install_ln vimrc
 install_ln gitconfig
+
