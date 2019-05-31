8d6cc458ad683c0d4d0c9e208d38ef4eabeef20a
sgz <219253+sangonz@users.noreply.github.com>
2019-05-31 20:06:19 +0200

    Add nvim

--- install.sh
+++ install.sh
@@ -12,13 +12,20 @@ function install_ln {
     file="$1"
     target=".dotfiles/$file"
     dest="$HOME/.$file"
+
+    # Make containing dirs
+    mkdir -p `dirname $dest`
+
     if [[ ! -e $dest ]]; then
+        # Create the link
         ln -s $target $dest
         printf "$P_NEW $dest\n"
     else
         if [[ $(readlink -e $dest) == $(readlink -e $file) ]]; then
+            # Link already exists and is correct
             printf "$P_CHK $dest\n"
         else
+            # Destination exists and does not link to dotfile's
             printf "$P_ERR $dest\n"
         fi
     fi
@@ -30,3 +37,6 @@ install_ln vim
 install_ln vimrc
 install_ln gitconfig
 
+for f in `find config -type f`; do
+    install_ln $f
+done
